0ab6261f8650efce1d300434f0f7fc41e455d86e
latteinthecloud <22520644@gm.uit.edu.vn>
2024-10-10 19:04:06 +0700

    add pj

---
+++ WebsiteQuanLySanPham/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteQuanLySanPham.Models;
+
+namespace WebsiteQuanLySanPham.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            string connection = "Server=localhost;Database=QuanLySanPham;Integrated Security=True;";
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(connection);
+            List<Catalog> dsCatalog = context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+    }
+}
---
+++ WebsiteQuanLySanPham/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteQuanLySanPham.Models;
+
+namespace WebsiteQuanLySanPham.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            string connection = "Server=localhost;Database=QuanLySanPham;Integrated Security=True;";
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(connection);
+            List<Product> dsProduct = context.Products.ToList();
+            return View(dsProduct);
+        }
+    }
+}
